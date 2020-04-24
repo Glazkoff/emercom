20ec384680c1f703881371f1f8105b764ea30a9d
Nikita Glazkov <zitrnik@gmail.com>
2020-04-25 00:26:18 +0300

    DEV: add frontend of new request editor

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const jwt = require("jsonwebtoken");
 const CONFIG = require("./secret.config");
 
 const app = express();
+const expressWs = require("express-ws")(app);
 
 // Парсинг json - application/json
 app.use(bodyParser.json());
@@ -17,7 +18,7 @@ app.use(bodyParser.json());
 // Парсинг запросов по типу: application/x-www-form-urlencoded
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
@@ -25,7 +26,7 @@ app.use(
 app.use("/", serveStatic(path.join(__dirname, "../dist")));
 
 // настройка CORS
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -39,33 +40,74 @@ app.use(function (req, res, next) {
 });
 
 // создаем соединение с нашей базой данных
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+//   charset: "utf8_general_ci"
+// });
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
-  charset: "utf8_general_ci"
+  charset: "utf8_general_ci",
 });
-try {
-  connection.connect(err => {
-    if (err) {
-      console.warn(err);
-    } else {
-      console.log(`Успешно соединено с базой данных ${dbConfig.HOST}`);
-      connection.query('SET NAMES "utf8"');
-      connection.query('SET CHARACTER SET "utf8"');
-      connection.query('SET SESSION collation_connection = "utf8_general_ci"');
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
     }
-  });
-} catch (err) {
-  console.warn(err);
-}
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  } else {
+    connection.query('SET NAMES "utf8"');
+    connection.query('SET CHARACTER SET "utf8"');
+    connection.query('SET SESSION collation_connection = "utf8_general_ci"');
+    console.log("Успешно соединено с БД");
+  }
+
+  if (connection) connection.release();
+  // return
+});
+
+// try {
+//   connection.connect(err => {
+//     if (err) {
+//       console.warn(err);
+//     } else {
+//       console.log(`Успешно соединено с базой данных ${dbConfig.HOST}`);
+//       connection.query('SET NAMES "utf8"');
+//       connection.query('SET CHARACTER SET "utf8"');
+//       connection.query('SET SESSION collation_connection = "utf8_general_ci"');
+//     }
+//   });
+// } catch (err) {
+//   console.warn(err);
+// }
 
 let salt = bcrypt.genSaltSync(10);
 
 /********************************************* */
 /***************** Ниже API ****************** */
 /********************************************* */
+
+// Обработка запросов протокола WebSockets
+app.ws("/ws", function(ws, req) {
+  ws.on("message", function(msg) {
+    console.log(ws);
+    console.log("WS:", msg);
+    ws.send(msg);
+  });
+});
 
 // Обработка прямого пути
 app.get("/", (req, res) => {
@@ -80,7 +122,7 @@ app.post("/api/register", (req, res) => {
   }
   let login = req.body.login;
   let password = bcrypt.hashSync(req.body.password, salt);
-  connection.query(
+  pool.query(
     "INSERT INTO `users` (fio, role, department_id, login, password) VALUES (?, ?, ?, ?, ?)",
     ["Глазков Никита Олегович", "admin", 0, login, password],
     (err, result) => {
@@ -90,7 +132,7 @@ app.post("/api/register", (req, res) => {
         return res.status(500).send("Проблема при регистрации пользователя");
       } else {
         console.log(result);
-        connection.query(
+        pool.query(
           "SELECT * FROM `users` WHERE login = ?",
           [login],
           (error, reslt) => {
@@ -100,17 +142,19 @@ app.post("/api/register", (req, res) => {
               return res.status(500).send("Проблема получении пользователя");
             } else {
               console.log(reslt);
-              let token = jwt.sign({
-                  id: reslt.insertId
+              let token = jwt.sign(
+                {
+                  id: reslt.insertId,
                 },
-                CONFIG.SECRET, {
-                  expiresIn: 86400 // токен на 24 часа
+                CONFIG.SECRET,
+                {
+                  expiresIn: 86400, // токен на 24 часа
                 }
               );
               res.status(200).send({
                 token,
                 fio: reslt[0].fio,
-                login: login
+                login: login,
               });
             }
           }
@@ -123,94 +167,96 @@ app.post("/api/register", (req, res) => {
 // Обработка авторизации
 app.post("/api/login", (req, res) => {
   console.log("LOGIN", req.body);
-  if (connection)
-    connection.query(
-      "SELECT * FROM `users` WHERE login = ?",
-      [req.body.login],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send("Ошибка на сервере");
+  // if (connection)
+  pool.query(
+    "SELECT * FROM `users` WHERE login = ?",
+    [req.body.login],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Ошибка на сервере");
+      } else {
+        if (!result[0]) {
+          res.status(404).send("Пользователь не найден");
         } else {
-          if (!result[0]) {
-            res.status(404).send("Пользователь не найден");
+          let passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            result[0].password
+          );
+          if (!passwordIsValid) {
+            return res.status(401).send({
+              token: null,
+            });
           } else {
-            let passwordIsValid = bcrypt.compareSync(
-              req.body.password,
-              result[0].password
+            let token = jwt.sign(
+              {
+                id: result[0].user_id,
+              },
+              CONFIG.SECRET,
+              {
+                expiresIn: 86400, // токен на 24 часа
+              }
             );
-            if (!passwordIsValid) {
-              return res.status(401).send({
-                token: null
-              });
-            } else {
-              let token = jwt.sign({
-                  id: result[0].user_id
-                },
-                CONFIG.SECRET, {
-                  expiresIn: 86400 // токен на 24 часа
-                }
-              );
-              res.status(200).send({
-                token,
-                fio: result[0].fio,
-                login: req.body.login
-              });
-            }
+            res.status(200).send({
+              token,
+              fio: result[0].fio,
+              login: req.body.login,
+            });
           }
         }
       }
-    );
+    }
+  );
 });
 
 //CRUD: requests
 app.post("/api/requests", (req, res) => {
-  if (connection) {
-    try {
-      connection.query(
-        "INSERT INTO `requests` ('status', 'user_id', 'department_id', 'executor_id', 'content') VALUES (?, ?, ?, ?, ?)",
-        [
-          req.body.status,
-          req.body.user_id,
-          req.body.department_id,
-          req.body.executor_id,
-          req.body.content
-        ],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-            res.status(500).send("Ошибка на сервере");
-          } else {
-            console.log("Результат запроса к БД:");
-            console.log(result);
-            res.send(result);
-          }
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-});
-
-app.get("/api/requests", (req, res) => {
-  if (connection) {
-    console.log("GET");
-    try {
-      connection.query("SELECT * FROM `requests`", (err, result) => {
+  // if (connection) {
+  try {
+    pool.query(
+      "INSERT INTO `requests` ('status', 'user_id', 'department_id', 'executor_id', 'content') VALUES (?, ?, ?, ?, ?)",
+      [
+        req.body.status,
+        req.body.user_id,
+        req.body.department_id,
+        req.body.executor_id,
+        req.body.content,
+      ],
+      (err, result) => {
         if (err) {
           console.log(err);
           res.status(500).send("Ошибка на сервере");
         } else {
           console.log("Результат запроса к БД:");
           console.log(result);
-          res.json(result);
+          res.send(result);
         }
-      });
-    } catch (error) {
-      console.log(error);
-    }
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
+  // }
+});
+
+app.get("/api/requests", (req, res) => {
+  // if (connection) {
+  console.log("GET");
+  try {
+    pool.query("SELECT * FROM `requests`", (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Ошибка на сервере");
+      } else {
+        console.log("Результат запроса к БД:");
+        console.log(result);
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  // }
 });
 
 // TODO: add put requests
@@ -219,55 +265,54 @@ app.get("/api/requests", (req, res) => {
 // TODO: add delete requests
 // app.delete("/api/requests", (req, res) => { });
 
-
 // CRUD: messages
 app.post("/api/messages", (req, res) => {
-  if (connection) {
-    try {
-      connection.query(
-        "INSERT INTO `messages` ('title', 'body', 'from_user_id', 'destination_id', 'type') VALUES (?, ?, ?, ?, ?)",
-        [
-          req.body.title,
-          req.body.body,
-          req.body.fromid,
-          req.body.toid,
-          req.body.status
-        ],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-            res.status(500).send("Ошибка на сервере");
-          } else {
-            console.log("Результат запроса к БД:");
-            console.log(result);
-            res.send(result);
-          }
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-});
-
-app.get("/api/messages", (req, res) => {
-  if (connection) {
-    console.log("GET");
-    try {
-      connection.query("SELECT * FROM `messages`", (err, result) => {
+  // if (connection) {
+  try {
+    pool.query(
+      "INSERT INTO `messages` ('title', 'body', 'from_user_id', 'destination_id', 'type') VALUES (?, ?, ?, ?, ?)",
+      [
+        req.body.title,
+        req.body.body,
+        req.body.fromid,
+        req.body.toid,
+        req.body.status,
+      ],
+      (err, result) => {
         if (err) {
           console.log(err);
           res.status(500).send("Ошибка на сервере");
         } else {
           console.log("Результат запроса к БД:");
           console.log(result);
-          res.json(result);
+          res.send(result);
         }
-      });
-    } catch (error) {
-      console.log(error);
-    }
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
+  // }
+});
+
+app.get("/api/messages", (req, res) => {
+  // if (connection) {
+  console.log("GET");
+  try {
+    pool.query("SELECT * FROM `messages`", (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Ошибка на сервере");
+      } else {
+        console.log("Результат запроса к БД:");
+        console.log(result);
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  // }
 });
 
 // TODO: add put messages
@@ -287,7 +332,7 @@ app.get("/test", (req, res) => {
     }
     console.log(decode);
     res.status(200).send({
-      id: decode.id
+      id: decode.id,
     });
   } else {
     res.status(401).send("Токен не найден!");
--- src/components/Write.vue
+++ src/components/Write.vue
@@ -1,21 +1,317 @@
 <template>
   <div class="main-screen">
-        <Sidepanel></Sidepanel>
-      <div class="main-panel">
+    <Sidepanel></Sidepanel>
+    <div class="main-panel">
+      <h2>Общие и личные оповещения</h2>
+      <div class="request-wrap">
+        <div class="request-main">
+          <h3>Новая заявка</h3>
+          <div class="input-form">
+            <label for="fio">ФИО</label>
+            <input type="text" id="fio" placeholder="Фамилия Имя Отчество" />
+            <small>Добавлено автоматически</small>
+          </div>
+          <div class="input-form">
+            <label for="departmentid">Номер отдела</label>
+            <input type="text" id="departmentid" placeholder="129301" />
+            <small>Добавлено автоматически</small>
+          </div>
+          <div class="technical-service">
+            <label for="">Устройства для тех.обслуживания</label>
+            <div class="devices-wrap">
+              <div class="devices-block">
+                <div class="device-title">МФУ #512383</div>
+                <div class="device-characteristics">
+                  <div class="characteristics-title">Характеристики</div>
+                  <div class="characteristics-text">
+                    Текст, текст, текст, текст. Текст, текст, текст, текст.
+                  </div>
+                </div>
+                <div class="input-form">
+                  <label for="servicetype">Тип обслуживания</label>
+                  <select name="servicetype" id="servicetype">
+                    <option value="">Заправка картриджа</option>
+                    <option value="">Обслуживание</option>
+                    <option value="">Устранение неполадок</option>
+                  </select>
+                  <!-- <small>Добавлено автоматически</small> -->
+                </div>
+                <div class="input-form">
+                  <label>Комментарий для специалиста</label>
+                  <textarea name="" placeholder="Ваш комментарий к устройству">
+                  </textarea>
+                  <!-- <small>Добавлено автоматически</small> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="input-form">
+            <label for="departmentid">Общий комментарий</label>
+            <textarea name="" placeholder="Ваш комментарий к заявке">
+            </textarea>
+            <!-- <small>Добавлено автоматически</small> -->
+          </div>
+          <div class="input-form">
+            <button>Отправить</button>
+          </div>
+        </div>
+        <div class="request-side">
+          <div class="devices-list-wrap">
+            <button>Добавить устройство</button>
+            <div class="devices-list-block">
+              <button class="list-btn">Добавить в заявку</button>
+              <div class="list-data">
+                <div class="data-title">ПК #512385</div>
+                <div class="data-description">
+                  <strong>Описание</strong>:<br />
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст. Текст, текст, текст, текст.
+                </div>
+              </div>
+            </div>
+            <div class="devices-list-block">
+              <button class="list-btn">Добавить в заявку</button>
+              <div class="list-data">
+                <div class="data-title">ПК #512385</div>
+                <div class="data-description">
+                  <strong>Описание</strong>:<br />
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст. Текст, текст, текст, текст. Текст, текст,
+                  текст, текст. Текст, текст, текст, текст. Текст, текст, текст,
+                  текст. Текст, текст, текст, текст. Текст, текст, текст, текст.
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст.
+                </div>
+              </div>
+            </div>
+            <div class="devices-list-block">
+              <button class="list-btn">Добавить в заявку</button>
+              <div class="list-data">
+                <div class="data-title">ПК #512385</div>
+                <div class="data-description">
+                  <strong>Описание</strong>:<br />
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст. Текст, текст, текст, текст.
+                </div>
+              </div>
+            </div>
+            <div class="devices-list-block">
+              <button class="list-btn">Добавить в заявку</button>
+              <div class="list-data">
+                <div class="data-title">ПК #512385</div>
+                <div class="data-description">
+                  <strong>Описание</strong>:<br />
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст. Текст, текст, текст, текст.
+                </div>
+              </div>
+            </div>
+            <div class="devices-list-block">
+              <button class="list-btn">Добавить в заявку</button>
+              <div class="list-data">
+                <div class="data-title">ПК #512385</div>
+                <div class="data-description">
+                  <strong>Описание</strong>:<br />
+                  Текст, текст, текст, текст. Текст, текст, текст, текст. Текст,
+                  текст, текст, текст. Текст, текст, текст, текст.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Sidepanel from '@/components/Sidepanel.vue'
+import Sidepanel from "@/components/Sidepanel.vue";
 export default {
-  name: 'Write',
+  name: "Write",
   components: {
-    Sidepanel
-  }
-}
+    Sidepanel,
+  },
+};
 </script>
 
 <style scoped>
+.main-panel {
+  overflow-y: scroll;
+  position: relative;
+  height: calc(100vh - 30px);
+  padding-bottom: 1rem;
+  box-sizing: border-box;
+}
+.main-panel h2 {
+  margin: 0;
+  padding: 1rem 1.5rem;
+}
+.request-wrap {
+  background-color: #c4c4c4;
+  margin: 0 auto;
+  border-radius: 8px;
+  width: 94%;
+  /* height: 40vh; */
+  min-height: 80vh;
+  border: 1px solid #000;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1.8fr 1fr;
+}
+.request-main h3 {
+  margin: 0;
+  padding: 1rem 1.2rem;
+}
+.request-side {
+  border-left: 1px solid #000;
+}
+.input-form,
+.technical-service {
+  margin-left: 2rem;
+  margin-bottom: 1rem;
+}
+.input-form label,
+.technical-service label {
+  padding: 0.3rem 0;
+  display: block;
+  font-family: "Montserrat Medium";
+}
+.input-form input[type="text"],
+.input-form select,
+.input-form textarea {
+  display: block;
+  box-sizing: border-box;
+  font-size: 1rem;
+  padding: 0.5rem 0.5rem;
+  border: 1px solid #000;
+  font-family: "Montserrat Regular";
+  resize: none;
+}
+.input-form textarea {
+  height: 5rem;
+}
+.input-form input[type="text"],
+.input-form textarea,
+.devices-wrap,
+button {
+  width: 70%;
+}
+.input-form small {
+  font-style: italic;
+}
 
+.devices-wrap {
+  border: 1px dashed #000;
+  box-sizing: border-box;
+  background-color: #f2f2f2;
+  padding: 0.8rem 1rem;
+}
+.devices-wrap .input-form {
+  margin-left: 0rem;
+}
+.devices-wrap .input-form input[type="text"] {
+  width: 90%;
+}
+.devices-wrap .input-form textarea,
+.devices-wrap .input-form select {
+  width: 100%;
+}
+.device-title {
+  font-family: "Montserrat Medium";
+  font-size: 1.2rem;
+  margin-bottom: 0.3rem;
+}
+.device-characteristics {
+  font-size: 0.8rem;
+  margin-top: 0.4rem;
+  margin-bottom: 0.4rem;
+}
+.characteristics-title {
+  font-weight: bold;
+}
+button {
+  background-color: #737373;
+  border: 0px;
+  border-radius: 8px;
+  font-family: "Montserrat Regular";
+  padding: 0.8rem;
+  font-size: 1rem;
+  color: #fff;
+  cursor: pointer;
+  outline: 0px;
+  transition: 0.02s;
+}
+button:hover {
+  background-color: #949393;
+}
+button:active {
+  background-color: #575757;
+  box-shadow: inset 0px 0px 10px rgba(0, 0, 0, 0.4);
+}
+.devices-list-wrap {
+  display: flex;
+  flex-direction: column;
+  padding: 0.6rem;
+  position: -webkit-sticky;
+  position: sticky;
+  z-index: 1;
+  top: 0;
+  height: 90vh;
+  overflow-y: scroll;
+}
+.devices-list-wrap > button {
+  width: 100%;
+  margin-bottom: 0.6rem;
+}
+.devices-list-block {
+  display: flex;
+  margin-bottom: 0.6rem;
+}
+.list-btn {
+  background: transparent;
+  width: 20%;
+  font-size: 0.6rem;
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+  color: #000;
+}
+.list-btn:hover {
+  background-color: transparent;
+  color: #fff;
+}
+.list-data {
+  background-color: #f2f2f2;
+  border-radius: 8px;
+  padding: 0.8rem;
+}
+.data-title {
+  font-family: "Montserrat Medium";
+  font-size: 1.2rem;
+  margin-bottom: 0.6rem;
+}
+.data-description {
+  font-size: 0.9rem;
+}
+::-webkit-scrollbar {
+  width: 12px;
+}
+::-webkit-scrollbar-thumb {
+  border-width: 1px 1px 1px 2px;
+  border-color: #777;
+  background-color: #aaa;
+  border-radius: 8px;
+  cursor: pointer;
+}
+::-webkit-scrollbar-thumb:hover {
+  border-width: 1px 1px 1px 2px;
+  border-color: #555;
+  background-color: #777;
+}
+::-webkit-scrollbar-track {
+  border-width: 0;
+}
+::-webkit-scrollbar-track:hover {
+  border-left: solid 1px #aaa;
+  background-color: #eee;
+}
 </style>
